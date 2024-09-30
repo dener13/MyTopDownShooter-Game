1052678ab1db3d7f97267e184c3b02cc7f318ac9
Dener Schenatto Moret <dener1307@gmail.com>
2024-09-30 13:28:52 -0300

    commit 11
    recorde pontos salvos na tela inicial

--- MyTopDownShooter Game/Assets/Scripts/GameOver e Menu/GameMenu.cs
+++ MyTopDownShooter Game/Assets/Scripts/GameOver e Menu/GameMenu.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class GameMenu : MonoBehaviour
 {
+
+    public TextMeshProUGUI highscoreTxt;
+
+    void Start()
+    {
+        // Carregar o recorde de pontos salvo e exibir
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);
+        highscoreTxt.text = "High Score: " + highScore.ToString();
+    }
 
     public void GoGame()
     {
